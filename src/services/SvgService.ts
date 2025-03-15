e470d43c81a66355b0bed224c9103b6726963b35
Jean-Pierre Costa Da Silva <dasilva.j.pierre@gmail.com>
2025-03-16 00:30:12 +0100

    SVGLoader as singleton

---
+++ src/services/SvgService.ts
@@ -0,0 +1,56 @@
+export class SVGService {
+  private static instance: SVGService;
+
+  /**
+   * Private constructor to prevent direct instantiation.
+   * Loads an SVG on initialization.
+   */
+  private constructor(private clockSVGElement: HTMLElement) {}
+  private static async construct(): Promise<SVGService> {
+    const clockSVGElement = await this.loadLocalSVG('assets/images/clock.svg');
+    return new SVGService(clockSVGElement);
+  }
+
+  /**
+   * Returns the singleton instance of SVGService.
+   * @returns SVGService instance.
+   */
+  public static getInstance(): SVGService {
+    if (!SVGService.instance) {
+      throw new Error('SVGService must be initalized before using it');
+    }
+    return SVGService.instance;
+  }
+
+  /**
+   * Instanciation of SVGService.
+   * @returns SVGService instance.
+   */
+  public static async initialize(): Promise<SVGService> {
+    if (!SVGService.instance) {
+      SVGService.instance = await SVGService.construct();
+    }
+    return SVGService.instance;
+  }
+
+  /**
+   * Loads an SVG file from a local path
+   * @param filePath - The path to the local SVG file
+   */
+  public static async loadLocalSVG(filePath: string): Promise<HTMLElement> {
+    // TODO(jpdcs): Handle possible errors
+    const response = await fetch(filePath);
+    const svgText = await response.text();
+    const parser = new DOMParser();
+    const svgDocument = parser.parseFromString(svgText, 'image/svg+xml');
+    return svgDocument.documentElement;
+  }
+
+  /**
+   * Getter for the loaded SVG element
+   * @returns A clone of the loaded SVG element.
+   */
+  public getClockSVGElement(): HTMLElement | null {
+    return this.clockSVGElement.cloneNode(true) as HTMLElement;
+  }
+}
